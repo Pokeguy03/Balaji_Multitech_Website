7d00c92b4d56ab27c4d4aed9409299f63cbe9eb6
GopikaJ15 <gopikaj1592005@gmail.com>
2026-01-03 21:07:05 +0530

    product and about section

---
+++ product and about section/script.js
@@ -0,0 +1,6 @@
+function handleBuy(productName) {
+    alert("Thank you for your interest in " + productName + "! Redirecting to checkout...");
+}
+
+// Log a message to verify the file is connected
+console.log("Balaji Multitech Website Loaded Successfully");
