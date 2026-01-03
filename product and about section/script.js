5210821e80d2ad025660eed4e1aa95792157f5b5
GopikaJ15 <gopikaj1592005@gmail.com>
2026-01-03 21:00:44 +0530

    Delete product and about section directory

--- product and about section/script.js
+++
@@ -1,6 +0,0 @@
-function handleBuy(productName) {
-    alert("Thank you for your interest in " + productName + "! Redirecting to checkout...");
-}
-
-// Log a message to verify the file is connected
-console.log("Balaji Multitech Website Loaded Successfully");
